af022c4a366309a36f869acc6e5d12c06bc2d2e5
Ángel Pérez <angelpxrz@gmail.com>
2020-12-20 10:56:23 +0100

    Firebase initial configuration

---
+++ src/components/Firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBNsMSsaouChDa6MSyzcj8LsKDYfNHWJ6U",
+    authDomain: "uluwatu-book-club.firebaseapp.com",
+    projectId: "uluwatu-book-club",
+    storageBucket: "uluwatu-book-club.appspot.com",
+    messagingSenderId: "823573162011",
+    appId: "1:823573162011:web:a505b6cfbbc46f1b3830be",
+    measurementId: "G-48NWNWMD7Q"
+};
+
+export default firebaseConfig;
