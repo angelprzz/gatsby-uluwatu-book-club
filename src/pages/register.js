65dc6849c461c6f8c3552d61a3a869c9c8d1d0a6
Ángel Pérez <angelpxrz@gmail.com>
2020-12-21 12:40:36 +0100

    Connect registration w/ Firebase

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,7 +1,9 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
 import { Form, Input, Button } from '../components/common'
+import {FirebaseContext} from '../components/Firebase'
 
 const Register = () => {
+    const {firebase} = useContext(FirebaseContext)
 
     const [formState, setFormState] = useState({
         email: '',
@@ -20,14 +22,21 @@ const Register = () => {
     function handleSubmit(e) {
         e.preventDefault();
 
+        if(formState.password === formState.confirmPassword){
+            firebase.register({
+                email: formState.email,
+                password: formState.password,
+            })
+        }
+
         console.log(formState)
     }
 
     return(
         <Form onSubmit={handleSubmit}>
             <Input onChange={handleInputChange} value={formState.email} placeholder="email" type="email" required name="email"/>
-            <Input onChange={handleInputChange} value={formState.password} placeholder="password" type="password" required minLength={3} name="password"/>
-            <Input onChange={handleInputChange} value={formState.confirmPassword} placeholder="confirm password" type="password" required minLength={3} name="confirmPassword"/>
+            <Input onChange={handleInputChange} value={formState.password} placeholder="password" type="password" required minLength={6} name="password"/>
+            <Input onChange={handleInputChange} value={formState.confirmPassword} placeholder="confirm password" type="password" required minLength={6} name="confirmPassword"/>
             <Button type="submit" block>
                 Register
             </Button>
