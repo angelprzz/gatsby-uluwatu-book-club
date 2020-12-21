c363a0dca9062d26256445cd9fc576a70e03aea8
Ángel Pérez <angelpxrz@gmail.com>
2020-12-21 17:17:58 +0100

    Display username

--- src/components/Firebase/useAuth.js
+++ src/components/Firebase/useAuth.js
@@ -17,45 +17,14 @@ function useAuth() {
 
             unsubscribe = firebaseInstance.auth.onAuthStateChanged(userResult => {
                 if (userResult) {
-                    setUser(userResult);
-                    // get user custom claims
-                    /*setLoading(true);
-                    Promise.all([
-                        firebaseInstance.getUserProfile({ userId: userResult.uid }),
-                        firebaseInstance.auth.currentUser.getIdTokenResult(true),
-                    ]).then((result) => {
-                        const publicProfileResult = result[0]
-                        const token = result[1]
-
-                        if (publicProfileResult.empty) {
-                            publicProfileUnsubscribe = firebaseInstance.db
-                              .collection("publicProfiles")
-                              .where("userId", "==", userResult.uid)
-                              .onSnapshot((snapshot) => {
-                                  const publicProfileDoc = snapshot.docs[0]
-                                  if (publicProfileDoc && publicProfileDoc.id) {
-                                      setUser({
-                                          ...userResult,
-                                          admin: token.claims.admin,
-                                          username: publicProfileDoc.id,
-                                      })
-                                  }
-
-                                  setLoading(false)
-                              })
-                        } else {
-                            const publicProfileDoc = publicProfileResult.docs[0]
-                            if (publicProfileDoc && publicProfileDoc.id) {
-                                setUser({
-                                    ...userResult,
-                                    admin: token.claims.admin,
-                                    username: publicProfileDoc.id,
-                                })
-                            }
-
-                            setLoading(false)
-                        }
-                    })*/
+                    firebaseInstance.getUserProfile({
+                        userId: userResult.uid
+                    }).then(r => {
+                        setUser({
+                            ...userResult,
+                            username: r.empty ? null : r.docs[0].id
+                        })
+                    })
                 }else{
                     setUser(null);
                 }
