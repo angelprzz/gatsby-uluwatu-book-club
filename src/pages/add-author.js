a3f7882044c98aa51a8a6d831077364b95e8bdda
Ángel Pérez <angelpxrz@gmail.com>
2020-12-27 18:17:24 +0100

    Submit author - Frontend

--- src/pages/add-author.js
+++ src/pages/add-author.js
@@ -1,21 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import {Form, Input, Button} from '../components/common'
+import { FirebaseContext } from '../components/Firebase'
 
 const AddAuthor = () => {
-
+    const {firebase} = useContext(FirebaseContext)
     const [authorName, setAuthorName] = useState('')
+    const [success, setSuccess] = useState(false)
 
     function handleSubmit(e) {
         e.preventDefault()
-
+        firebase.createAuthor({
+            authorName
+        }).then(() => {
+            setAuthorName('')
+            setSuccess(true);
+        })
     }
 
     return(
         <Form onSubmit={handleSubmit}>
             <Input onChange={(e) => {
                 e.persist();
+                setSuccess(false)
                 setAuthorName(e.target.value)
             }} value={authorName} placeolder="author name" />
+            {!!success &&
+                <span>
+                    Author created successfully.
+                </span>
+            }
             <Button type="submit" block>
                 Add new author
             </Button>
