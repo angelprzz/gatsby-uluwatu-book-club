a4efa012e0fb741ba14fbcb962894ec3401dfdc8
Ángel Pérez <angelpxrz@gmail.com>
2020-12-28 16:06:30 +0100

    Tweak Gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,49 +1,46 @@
-const path = require('path')
+const path = require("path")
 
 exports.onCreateWebpackConfig = ({ actions, stage }) => {
-  if (stage === "develop-html" || stage === "build-html") {
-    actions.setWebpackConfig({
-      resolve: {
-        mainFields: ["main"],
-      },
-    })
-  } else {
-    actions.setWebpackConfig({
-      resolve: {
-        mainFields: ["browser", "module", "main"],
-      },
-    })
-  }
+    if (stage === "develop-html" || stage === "build-html") {
+        actions.setWebpackConfig({
+            resolve: {
+                mainFields: ["main"],
+            },
+        })
+    } else {
+        actions.setWebpackConfig({
+            resolve: {
+                mainFields: ["browser", "module", "main"],
+            },
+        })
+    }
 }
 
-exports.createPages = ({graphql, actions}) => {
-    const {createPage} = actions
-    const bookTemplate = path.resolve('src/templates/bookTemplate.js')
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+    const bookTemplate = path.resolve("src/templates/bookTemplate.js")
 
     return graphql(`
-        query {
-          allBook {
-            edges {
-              node {
-                id
-                author {
-                  name
-                }
-              }
-            }
+    {
+      allBook {
+        edges {
+          node {
+            id
           }
         }
-    `).then((result) => {
-        if(result.errors) {
-            throw result.errors;
+      }
+    }
+  `).then(result => {
+        if (result.errors) {
+            throw result.errors
         }
 
         result.data.allBook.edges.forEach(book => {
             createPage({
                 path: `/book/${book.node.id}`,
                 component: bookTemplate,
-                context: {bookId: book.node.id}
+                context: { bookId: book.node.id },
             })
-        });
+        })
     })
 }
